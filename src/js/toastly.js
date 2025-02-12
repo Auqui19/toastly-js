20f1efd0a2a49ab5ee2354913d7b9d010812b985
Alonso Auqui <andriod.pvp@gmail.com>
2025-02-11 22:44:13 -0500

    📌 Mejoras

--- src/js/toastly.js
+++ src/js/toastly.js
@@ -1,84 +1,76 @@
 class Toastly {
-  constructor() {
-    this.toast = document.getElementById("toastly");
-    if (!this.toast) {
-      this.toast = document.createElement("div");
-      this.toast.id = "toastly";
-      this.toast.className = "toastly hidden";
-      document.body.appendChild(this.toast);
-    }
+  constructor({
+    text = "",
+    type = "dark",
+    duration = 3000,
+    bgColor = null,
+  } = {}) {
+    this.createToastElement();
+    this.timeoutId = null;
 
-    this.toastMessage = document.getElementById("toast-message");
-    if (!this.toastMessage) {
-      this.toastMessage = document.createElement("p");
-      this.toastMessage.id = "toast-message";
-      this.toast.appendChild(this.toastMessage);
+    if (text) {
+      this.show({ text, type, duration, bgColor });
     }
-
-    this.closeButton = document.getElementById("toast-close");
-    if (!this.closeButton) {
-      this.closeButton = document.createElement("button");
-      this.closeButton.id = "toast-close";
-      this.closeButton.className = "toastly-close";
-      this.closeButton.innerHTML = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>`;
-      this.closeButton.addEventListener("click", () => this.hide());
-      this.toast.appendChild(this.closeButton);
-    }
-
-    this.timeouId = null;
   }
 
-  show(message, type = "info", duration = 3000, bgColor = null) {
-    if (this.timeouId) {
-      clearTimeout(this.timeouId);
+  createToastElement() {
+    this.toast = document.createElement("div");
+    this.toast.className = "toastly hidden";
+
+    this.toastMessage = document.createElement("p");
+    this.toastMessage.className = "toast-message";
+    this.toast.appendChild(this.toastMessage);
+
+    this.closeButton = document.createElement("button");
+    this.closeButton.className = "toastly-close";
+    this.closeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12"/><path d="M6 6l12 12"/></svg>`;
+    this.closeButton.addEventListener("click", () => this.hide());
+
+    this.toast.appendChild(this.closeButton);
+
+    let toastContainer = document.getElementById("toast-container");
+    if (!toastContainer) {
+      toastContainer = document.createElement("div");
+      toastContainer.id = "toast-container";
+      toastContainer.style.position = "fixed";
+      toastContainer.style.top = "20px";
+      toastContainer.style.right = "20px";
+      toastContainer.style.zIndex = "1000";
+      toastContainer.style.display = "flex";
+      toastContainer.style.flexDirection = "column";
+      toastContainer.style.gap = "10px";
+      document.body.appendChild(toastContainer);
     }
 
-    this.toast.classList.remove(
-      "success",
-      "error",
-      "info",
-      "warning",
-      "show",
-      "hide"
-    );
+    toastContainer.appendChild(this.toast);
+  }
 
-    this.toast.classList.add(type);
+  show({ text, type = "info", duration = 3000, bgColor = null }) {
+    this.toast.classList.add(type, "show");
 
     if (bgColor) {
       this.toast.style.backgroundColor = bgColor;
       this.toast.style.boxShadow = `0 0px 10px ${bgColor}`;
-    } else {
-      this.toast.style.backgroundColor = "";
-      this.toast.style.boxShadow = "";
     }
 
-    this.toastMessage.textContent = message;
-
-    // Mostrar el toast
+    this.toastMessage.textContent = text;
     this.toast.classList.remove("hidden");
-    this.toast.classList.add("show");
 
     if (duration > 0) {
-      this.timeouId = setTimeout(() => this.hide(), duration);
+      this.timeoutId = setTimeout(() => this.hide(), duration);
     }
   }
-  hide() {
-    // Ocultar el toast
-    this.toast.classList.remove("show");
-    this.toast.classList.add("hide");
 
+  hide() {
+    this.toast.classList.add("hide");
     this.toast.addEventListener(
       "animationend",
       () => {
-        this.toast.classList.remove("hide");
-        this.toast.classList.add("hidden");
-        this.toastMessage.textContent = "";
-        this.toast.style.backgroundColor = "";
-        this.toast.style.boxShadow = "";
+        this.toast.remove();
       },
       { once: true }
     );
   }
 }
 
-window.Toastly = Toastly;
+window.Toastly = (options) => new Toastly(options);
